701acf8f55c7052798ecddf999c1b79d683ed374
Michael Fiebelkorn <michael.fiebelkorn@gmail.com>
2024-12-08 16:51:49 +0100

    firebase link fixed

--- scripts/module.js
+++ scripts/module.js
@@ -3,7 +3,7 @@
 */
 import './boards.js';
 import './add-task.js';
-export const FIREBASE_URL = 'https://join-378-default-rtdb.europe-west1.firebasedatabase.app/';
+export const FIREBASE_URL = 'https://join-2024-10-09-default-rtdb.europe-west1.firebasedatabase.app';
 export const USERS_DIR = '/users';
 export const CONTACTS_DIR = '/contacts';
 export const TASKS_DIR = '/tasks';
